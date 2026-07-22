9d3ded5ed3461c6b3dab07cb3fb01ed74e2f4f33
aoqia <aoqia@aoqia.dev>
2026-07-22 18:37:37 +1000

    build: update jreleaser also disable stricter validation like upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -225,10 +225,6 @@ tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
 
-tasks.validatePlugins {
-    enableStricterValidation = true
-}
-
 tasks.register("downloadGradleSources") {
     description = "Downloads the Gradle API sources next to the API jar." +
         "May require you to manually attach the sources jar."
